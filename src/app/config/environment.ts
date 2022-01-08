b6c6894f0517a5688db6f3ec43d65f340bded927
ps-pr <62734970+ps-pr@users.noreply.github.com>
2022-01-08 18:56:50 +0530

    configremoved

--- src/app/config/environment.ts
+++ src/app/config/environment.ts
@@ -3,12 +3,6 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'AIzaSyCZNoVU2GzojzoL-sBbHLldaA1t7g7_M1U',
-    authDomain: 'mailapp-7c27b.firebaseapp.com',
-    databaseURL: 'https://mailapp-7c27b-default-rtdb.firebaseio.com',
-    projectId: 'mailapp-7c27b',
-    storageBucket: 'mailapp-7c27b.appspot.com',
-    messagingSenderId: '859632771451',
-    appId: '1:859632771451:web:cdd5262f6826075a8b1dde',
+   /***firebase coinfig object here ****?
   },
 };
